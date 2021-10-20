4c8e2e77f94f0c5c692e2ec15151e623b626ffcd
ArtisanSelects <85949213+ArtisanSelects@users.noreply.github.com>
2021-10-20 16:32:08 -0400

    Hardcoded baseURL in http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    const baseUrl = "http://localhost:5000/api/v1/scores";
-} else { 
-    const baseUrl = "https://ddr-score-manager.herokuapp.com/api/v1/scores";
-}
-
 export default axios.create({
-    baseURL: baseUrl,
+    baseURL: "https://ddr-score-manager.herokuapp.com/api/v1/scores",
     withCredentials: true,
     headers: {
         "Content-type": "application/json",  
