bf269cdc81075cb04609fa1026216698d77ccc90
ArtisanSelects <85949213+ArtisanSelects@users.noreply.github.com>
2021-10-20 16:43:17 -0400

    Changed withCredentials to false in http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "https://ddr-score-manager.herokuapp.com/api/v1/scores",
-    withCredentials: true,
+    withCredentials: false,
     headers: {
         "Content-type": "application/json",  
     }
